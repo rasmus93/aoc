7dfc38d2c2ede7c98fc9fed3a7159970f6aee973
Sergii Balganbaiev <sergiib@wix.com>
2021-12-07 07:21:50 +0200

    Solutions for day 7

---
+++ 2021/7/part1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, n) for (int i=0; i<n; ++i)
+#define FORE(i, n) for (int i=0; i<=n; ++i)
+#define REP(i, a, b) for (int i=a; i<b; ++i)
+#define REPE(i, a, b) for (int i=a; i<=b; ++i)
+#define mp make_pair
+#define pb push_back
+
+typedef long double dbl;
+typedef pair<int, int> PII;
+typedef vector<PII> VPII;
+typedef long long int ll;
+typedef vector<int> VI;
+typedef vector<bool> VB;
+typedef vector<VI> VVI;
+
+bool pred(const int &i, const int &j) {
+    return i < j;
+}
+
+int main() {
+    // g++ main.cpp -std=c++17 -pthread -O3 -o Solution
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    string str;
+    getline(cin, str);
+    stringstream ss(str);
+    string v;
+    VI a;
+    while (getline(ss, v, ',')) {
+        a.pb(atoi(v.c_str()));
+    }
+    int res = (int)1e9;
+    FOR(i,2000) {
+        int now = 0;
+        FOR(j,a.size()) {
+            now += abs(a[j] - i);
+        }
+        res = min(now,res);
+    }
+    cout << res;
+
+    return 0;
+}
